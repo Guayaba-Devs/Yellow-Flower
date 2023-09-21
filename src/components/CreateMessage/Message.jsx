14a7a44aba431b90b4840b5b76b37e674c5700e5
josafat <josa221001@gmail.com>
2023-09-21 11:20:26 -0600

    add: vista del link

--- src/components/CreateMessage/Message.jsx
+++ src/components/CreateMessage/Message.jsx
@@ -1,37 +1,88 @@
-import { useState } from "react"
-import { Link } from "react-router-dom"
-
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
 
 export const Message = () => {
+  const url = window.location.origin;
+  const { pathname } = useLocation();
 
-    const [message, setMessage] = useState('')
+  const [message, setMessage] = useState("");
+  const [messageUrl, setMessageUrl] = useState("");
+  const [copied, setCopied] = useState(false);
 
-    const handleMessageChange = (e) => {
-        setMessage(e.target.value)
-    }
+  const handleMessageChange = async (e) => {
+    await setMessage(e.target.value);
+  };
 
-    const generateLink = (e) => {
-        e.preventDefault();
-        console.log('generating link', message)
-    }
+  const generateLink = (e) => {
+    e.preventDefault();
+    const generatedUrl = `${url}/message/${btoa(message)}`;
+    console.log(generatedUrl);
+    setMessageUrl(generatedUrl);
+  };
 
-    return (
-        <div>
-            <h1 className='text-3xl font-bold mb-6  '>Envia una flor amarilla!
-                <span role='img' aria-label='flower'>🌻</span>
-            </h1>
+  const copyToClipboard = (e) => {
+    e.preventDefault()
+    const textArea = document.createElement("textarea");
+    textArea.value = messageUrl;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000); 
+  };
 
-            <p className='text-xl mb-3'>Escribe un mensaje y envíalo a tu persona favorita!</p>
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-6">
+        Envía una flor amarilla!
+        <span role="img" aria-label="flower">
+          🌻
+        </span>
+      </h1>
 
-            <form action="" className="w-full flex items-center justify-center flex-col">
+      <p className="text-xl mb-3">
+        Escribe un mensaje y envíalo a tu persona favorita!
+      </p>
 
-                <textarea className='border-2 border-yellow-300 rounded-lg p-2 w-full h-24 mt-2 max-w-lg mb-3' onChange={handleMessageChange} placeholder="Ingresa tu mensaje para esa persona :D"/>
+      <form action="" className="w-full flex items-center justify-center flex-col">
+        <textarea
+          className="border-2 border-yellow-300 rounded-lg p-2 w-full h-24 mt-2 max-w-lg mb-3"
+          onChange={handleMessageChange}
+          placeholder="Ingresa tu mensaje para esa persona :D"
+        />
+        <button
+          className="bg-yellow-300 hover:bg-yellow-400 text-black rounded-lg px-4 py-2 mt-2 w-32"
+          onClick={generateLink}
+        >
+          Generar Link
+        </button>
+        {messageUrl && (
+          <button
+            className="bg-green-500 hover:bg-green-600 text-white rounded-lg px-4 py-2 mt-2 w-32"
+            onClick={copyToClipboard}
+          >
+            {copied ? "Copiado" : "Copiar el link"}
+          </button>
+        )}
+      </form>
 
-                <button className='bg-yellow-300 hover:bg-yellow-400 text-black rounded-lg px-4 py-2 mt-2 w-32' onClick={generateLink}>Generar Link</button>
+      <div className="mt-5">
+        {messageUrl && (
+          <p className="bg-slate-600 max-w-lg mx-auto rounded-sm p-4 overflow-hidden">
+            URL generada:{" "}
+            <a
+              className="bg-stone-400 overflow-x-scroll"
+              href={messageUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {messageUrl}
+            </a>
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
 
-            </form>
-
-
-        </div>
-    )
-}
--- src/components/FlowerVisualize/Flower.jsx
+++ src/components/FlowerVisualize/Flower.jsx
@@ -1,16 +1,40 @@
-import { useState } from "react"
-
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 export const Flower = () => {
+  const [decodedMessage, setDecodedMessage] = useState("");
+  const { message } = useParams();
 
-    return (
-        <div>
-            <h1 className='text-3xl font-bold mb-6  '>Envia una flor amarilla!
-                <span role='img' aria-label='flower'>🌻</span>
-            </h1>
+  useEffect(() => {
+    setDecodedMessage(atob(message));
+  }, [message]);
 
-            <p className='text-xl mb-3'>Escribe un mensaje y envíalo a tu persona favorita!</p>
+  return (
+    <div className="h-full">
+        <h1 className="text-3xl font-bold mb-6">
+            Alguien que te quiere te envio una flor!
+            <span role="img" aria-label="flower">
+            🌻
+            </span>
+        </h1>
 
+        <img 
+            className="w-full max-w-lg mx-auto rounded-lg" 
+            src="https://www.bloomberglinea.com/resizer/-duYn76ZQbqQ4X1XzJkLSOxu7p0=/600x0/filters:format(webp):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/bloomberglinea/NJKR6OXHYZA5TKBTNL66O5BM3A.jpg" 
+            alt="imagen de flores" 
+        />
+
+        <div className="w-full my-5 text-start">
+            <h3 className="w-full text-start text-2xl mb-1">Y un mensaje...</h3>
+            <p className="text-xl">
+                {decodedMessage}
+            </p>
         </div>
-    )
-}
+
+
+        <p className="">
+            Desarrollado por <a className="text-teal-400 hover:text-teal-600" href="https://github.com/Escihu-Wizards" target="_blank" rel="noopener noreferrer">Escihu-Wizards</a>
+        </p>
+    </div>
+  );
+};
